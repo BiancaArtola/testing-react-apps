b0ad3700241dfb4fef99ff5daa13aab3933b297d
BiancaArtola <bian.artola@hotmail.com>
2022-08-29 17:19:31 -0300

    Complete exercise 4

--- src/__tests__/exercise/04.extra-2.js
+++ src/__tests__/exercise/04.extra-2.js
@@ -5,23 +5,30 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+
+function buildLoginForm() {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', async () => {
-  let submittedData
-  const handleSubmit = data => (submittedData = data)
+  const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
 
-  const username = 'chucknorris'
-  const password = 'i need no password'
+  const {username, password} = buildLoginForm()
 
   await userEvent.type(screen.getByLabelText(/Username/i), username)
   await userEvent.type(screen.getByLabelText(/Password/i), password)
   await userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  expect(submittedData).toEqual({
+  expect(handleSubmit).toHaveBeenCalledWith({
     username: username,
     password: password,
   })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
 /*
